222c326383f3786ac5b06e5f2ab5aef79888e77c
junwei99555 <junwei10255@gmail.com>
2023-07-31 17:11:58 +0800

    hehe3

--- user_api.py
+++ user_api.py
@@ -368,9 +368,9 @@ def webhook():
         pull_info = origin.pull()
 
         if len(pull_info) == 0:
-            return json.dumps({'msg': "Didn't pull any information from remote!"})
+            return json.dumps({'msg': "Didn't pull any information from remote!"}), 459
         if pull_info[0].flags > 128:
-            return json.dumps({'msg': "Didn't pull any information from remote!"})
+            return json.dumps({'msg': "Didn't pull any information from remote!"}), 460
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
